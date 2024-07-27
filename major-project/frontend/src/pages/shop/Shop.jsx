492d3960e0974417e92747d8a81235a2064bf170
Shivansh Pandey <shivp4uu@gmail.com>
2024-07-27 15:46:02 +0530

    Fifth Commit

--- major-project/frontend/src/pages/shop/Shop.jsx
+++ major-project/frontend/src/pages/shop/Shop.jsx
@@ -16,7 +16,7 @@ const Shop = () => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          `https://order-food-with-mern.vercel.app/api/products`
+          `https://order-food-with-mern.vercel.app/api/dishes`
         );
         console.log(response.data);
         setProduct(response.data);
